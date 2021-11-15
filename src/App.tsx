a81a4714bbc004ad3f78225ca96c4efa65ab2461
Алексей <alexeybudai@gmail.com>
2021-11-15 23:07:32 +0300

    Lessons_2 Monday(learning useState)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
+
+export type filterType = 'All' | 'Active' | 'Completed'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [task1, setTask] = useState([
+        {id: 1, title: "Hello world11111", isDone: true},
+        {id: 2, title: "I am Happy11111", isDone: false},
+        {id: 3, title: "Yo11111", isDone: false}],)
+    const removeTask = (mId: number) => {
+        setTask(task1.filter(f => f.id !== mId))
+    }
+    const [filter, setMyFilter] = useState<filterType>('All')
+    const setFilter = (idFilter: filterType) => {
+        setMyFilter(idFilter)
+    }
+    let filterTask = filter === 'Active' ? task1.filter(f => f.isDone) :
+        filter === 'Completed' ? task1.filter(f => !f.isDone) : task1
+    console.log(filterTask)
+    return (
+        <div className="App">
+            <Todolist
+                title={"Hello world11111"}
+                tasks={filterTask}
+                remove={removeTask}
+                setFilter={setFilter}/>
+        </div>
+    );
 }
 
 export default App;
