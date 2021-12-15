ac57e0ede39b1c4801794f4f81f7ef6e3dd634e9
Алексей <alexeybudai@gmail.com>
2021-12-15 13:57:28 +0300

    Lessons_6 Tuesday version by Igor up callBackForEditTaskHandler

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,6 +1,7 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import React, {ChangeEvent, useState, KeyboardEvent} from 'react';
 import {FilterValuesType} from './App';
-import MapTasks from "./MapTasks";
+import SuperInput from "./SuperInput";
+import EditTask from "./components/EditTask";
 
 export type TaskType = {
     id: string
@@ -9,83 +10,81 @@ export type TaskType = {
 }
 
 type PropsType = {
-    id:string
+    id: string
     title: string
     tasks: Array<TaskType>
-    removeTask: (taskId: string,todoList:string) => void
-    changeFilter: (todoListId:string,value: FilterValuesType) => void
-    addTask: (todoListId:string, title: string) => void
-    changeTaskStatus: (todoListId:string,taskId: string, isDone: boolean) => void
+    removeTask: (taskId: string, todolistId: string) => void
+    changeFilter: (value: FilterValuesType, todolistId: string) => void
+    addTask: (title: string, todolistId: string) => void
+    changeTaskStatus: (id: string, isDone: boolean, todolistId: string) => void
+    removeTodolist: (id: string) => void
     filter: FilterValuesType
+    updateTask: (todoListId: string, id: string , localTitle: string) => void
+    updateTitle: (todoListId: string, localTitle: string) => void
 }
 
 export function Todolist(props: PropsType) {
-
-    let [title, setTitle] = useState("")
-    let [error, setError] = useState<string | null>(null)
-
-    const addTask = () => {
-        if (title.trim() !== "") {
-            props.addTask(props.id,title.trim());
-            setTitle("");
-        } else {
-            setError("Title is required");
+    const removeTodolist = () => props.removeTodolist(props.id)
+    const onAllClickHandler = () => props.changeFilter("all", props.id);
+    const onActiveClickHandler = () => props.changeFilter("active", props.id);
+    const onCompletedClickHandler = () => props.changeFilter("completed", props.id);
+    const onChangeHandler = (title: string) => {
+        props.addTask(title, props.id)
+    }
+    const callBackForEditTask = (localTitle: string) => {
+        props.updateTitle(props.id, localTitle)
+    }
+    const callBackForEditTaskHandler = (localTitle: string, id?: string ) => {
+        if(id){
+            props.updateTask(props.id, id, localTitle)
         }
     }
 
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setTitle(e.currentTarget.value)
-    }
-
-    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        setError(null);
-        if (e.charCode === 13) {
-            addTask();
-        }
-    }
-
-    const onAllClickHandler = () => props.changeFilter(props.id,"all");
-    const onActiveClickHandler = () => props.changeFilter(props.id,"active");
-    const onCompletedClickHandler = () => props.changeFilter(props.id,"completed");
 
 
     return <div>
-        <h3>{props.title}</h3>
-        <div>
-            <input value={title}
-                   onChange={onChangeHandler}
-                   onKeyPress={onKeyPressHandler}
-                   className={error ? "error" : ""}
-            />
-            <button onClick={addTask}>+</button>
-            {error && <div className="error-message">{error}</div>}
-        </div>
-      <MapTasks tasks={props.tasks} removeTask={props.removeTask} id={props.id} changeTaskStatus={props.changeTaskStatus}/>
-        {/* <ul>
+        <h3>
+            <EditTask title={props.title} callBackForEditTask={callBackForEditTask}/>
+            <button onClick={removeTodolist}>x</button>
+        </h3>
+        <SuperInput onChangeHandler={onChangeHandler}/>
+        <ul>
             {
                 props.tasks.map(t => {
-                    const onClickHandler = () => props.removeTask(t.id,props.id)
+                    const onClickHandler = () => props.removeTask(t.id, props.id)
                     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-                        props.changeTaskStatus(props.id, t.id, e.currentTarget.checked);
+                        let newIsDoneValue = e.currentTarget.checked;
+                        props.changeTaskStatus(t.id, newIsDoneValue, props.id);
+
                     }
+                    /*const callBackForEditTaskHandler=(localTitle:string)=>{
+                         props.updateTask(props.id,t.id,localTitle)
+                     }*/
 
                     return <li key={t.id} className={t.isDone ? "is-done" : ""}>
-                        <input type="checkbox"
-                               onChange={onChangeHandler}
-                               checked={t.isDone}/>
-                        <span>{t.title}</span>
+                        <input type="checkbox" onChange={onChangeHandler} checked={t.isDone}/>
+                        <EditTask title={t.title}
+                                  callBackForEditTask={callBackForEditTaskHandler}
+                                  id={t.id}
+                        />
+                        {/*callBackForEditTask={()=>callBackForEditTaskHandler(props.id,t.id,t.title)}/>*/}
                         <button onClick={onClickHandler}>x</button>
                     </li>
                 })
             }
-        </ul>*/}
+        </ul>
         <div>
             <button className={props.filter === 'all' ? "active-filter" : ""}
-                    onClick={onAllClickHandler}>All</button>
+                    onClick={onAllClickHandler}>All
+            </button>
             <button className={props.filter === 'active' ? "active-filter" : ""}
-                onClick={onActiveClickHandler}>Active</button>
+                    onClick={onActiveClickHandler}>Active
+            </button>
             <button className={props.filter === 'completed' ? "active-filter" : ""}
-                onClick={onCompletedClickHandler}>Completed</button>
+                    onClick={onCompletedClickHandler}>Completed
+            </button>
         </div>
     </div>
 }
+
+
